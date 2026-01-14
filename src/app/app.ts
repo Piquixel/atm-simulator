7fc11ce718e8008a84185e359a2f84d9e10b35af
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-01-14 20:04:31 +0100

    first part

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Toolbar } from './components/toolbar/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Toolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('atm');
+  public readonly appName = "My super ATM App !"
 }
---
+++ src/app/components/toolbar/toolbar.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.scss',
+})
+export class Toolbar {
+  public readonly title = input<string>();
+}
