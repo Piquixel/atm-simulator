91cad3cce13b4793c88b7852661da5290ffe48eb
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-01-26 12:47:40 +0100

    Pin feature

--- src/app/components/pin-pad/pin-pad.ts
+++ src/app/components/pin-pad/pin-pad.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-pin-pad',
-  imports: [],
+  imports: [MatGridList, MatButtonModule, MatGridTile],
   templateUrl: './pin-pad.html',
   styleUrl: './pin-pad.scss',
 })
 export class PinPad {
+  public readonly tiles = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'X', '0', 'V'];
+  public pin = '';
 
+  handleClick(value: string): void {
+    switch (value) {
+      case 'X':
+        this.pin = '';
+        break;
+      case 'V':
+      default: {
+        if (this.pin.length < 4) {
+          this.pin += value;
+        }
+        break;
+      }
+    }
+  }
 }
