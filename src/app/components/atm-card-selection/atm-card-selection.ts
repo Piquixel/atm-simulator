a6782aaea79d53947670e188bb1b02daecf6e4a1
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-01-27 21:03:46 +0100

    add card

--- src/app/components/atm-card-selection/atm-card-selection.ts
+++ src/app/components/atm-card-selection/atm-card-selection.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CUSTOMERS } from '../../models/data/customers.mock';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-atm-card-selection',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './atm-card-selection.html',
   styleUrl: './atm-card-selection.scss',
 })
