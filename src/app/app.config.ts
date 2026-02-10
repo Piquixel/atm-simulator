f09e5f4486c1a0e87fe0523dea7ada53149f0c1c
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-02-10 18:37:11 +0100

    Update locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import localeFr from '@angular/common/locales/fr';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr, 'fr-BE');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'fr-BE' },
+  ],
 };
